95b651c1ce91c08d77a280c8b0f9e19b968eac26
Fahad Mohammad <78467016+fahadm23@users.noreply.github.com>
2025-09-23 23:51:20 -0400

    Day 02 - Tip Calculator

---
+++ day02/notes.md
@@ -0,0 +1,7 @@
+# Day 2 – Tip Calculator 💸
+
+✅ Learned:
+-Different Data Types: String, Integer, Large Integer, Float, Boolean
+-Type function: Returns type of data type e.g Type("Hello")
+-f strings: Make it easy to concatenate variables using e.g. f"{username}
+-cannot concatenate string and integer of other type, must convert first.
---
+++ day02/tip_calculator.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator!")
+total_bill = float(input("What was the total bill?"))
+tip_percentage = float(input("How much tip would you like to give? 10, 12, or 15?"))
+amount_of_people = int(input("How many peoplle to split the bill?"))
+tip_amount = float((total_bill * (tip_percentage / 100) + total_bill) / amount_of_people)
+print(f"Each person should pay: $ {round(tip_amount, 2)}")
